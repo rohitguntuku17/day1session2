a7ca52fd2a785ec8786195f0de3445ef196f7271
guntuku rohit <1210416417@gitam.in>
2020-07-15 23:20:07 +0530

    first commit

---
+++ day1session2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace product
+{
+    class product
+    {
+        static void Main(string[] args)
+        {
+             internal int productId;
+        internal string brand;
+        internal string model;
+        internal double price;
+        internal string description;
+
+        internal void Display()
+        {
+            Console.WriteLine("ID:" + productId + " " + brand + "-" + model + " Rs." + price + " " + description);
+        }
+
+
+        internal Product()
+        {
+            Console.WriteLine("Empty Object Initialization");
+        }
+
+
+        internal Product(int productId, string brand, string model, double price, string description)
+        {
+            Console.WriteLine("Object Initialized");
+            this.productId = productId;
+            this.brand = brand;
+            this.model = model;
+            this.description = description;
+            this.price = price;
+        } 
+    }
+}
---
+++ day1session2/program2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day1session2
+{
+    class program2
+    {
+        int x = 100;
+
+
+        Product product1; 
+        product1 = new Product();
+
+        //assigning values to attributes
+        product1.productId = 1001;
+            product1.price = 78000;
+            product1.brand = "iphone";
+            product1.description = "available in all colors";
+            product1.model = "X";
+
+          
+            product1.Display();
+
+           
+            Product product2 = new Product(1002, "iphone", "8", 55000, "available in black and blue colors only");
+        product2.Display();
+
+            product1 = new Product();
+        product1.Display();
+
+
+        }
+}
